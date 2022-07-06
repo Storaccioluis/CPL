5ef43eacf4d54d9c5b7bac57a7eb8ee313ca9e22
Luis Sebastián Storaccio <storaccioluis@gmail.com>
2022-07-06 18:30:57 -0300

    Primer commit

---
+++ TP1/script.sh
@@ -0,0 +1,10 @@
+ghdl -s "nombre".vhdl
+ghdl -s "nombre_tb".vhdl
+ghdl -a "nombre".vhdl
+ghdl -a "nombre_tb".vhdl
+ghdl -e "nombre_tb"
+ghdl -r "nombre_tb"
+ghdl -r "nombre_tb" --vcd ="nombre".vcd
+
+
+
